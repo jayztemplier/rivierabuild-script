5aed3f442e19436c0d8cd0094fddda76fa1a73e4
Jeremy Templier <templier.jeremy@gmail.com>
2016-04-04 23:43:58 -0700

    update script to support new API

--- script-example.sh
+++ script-example.sh
@@ -1,6 +1,9 @@
-PASSWORD="YOUR PASSWORD"
+API_KEY="you-api-key"
+APP_ID="your-app-id" #example: 1
+PASSWORD="your-password"
 AVAILABILITY="10_minutes" #10_minutes, 1_hour, 3_hours, 6_hours, 12_hours, 24_hours, forever
 TMP_FILE_PATH="/tmp/${PRODUCT_NAME}.ipa"
+
 GROWL="${HOME}/bin/growlnotify -a Xcode -w"
 
 echo "Creating .ipa for ${PRODUCT_NAME}" | ${GROWL}
@@ -9,7 +12,7 @@ echo "Created .ipa for ${PRODUCT_NAME}" | ${GROWL}
 
 
 echo "Uploading .ipa to RivieraBuild" | ${GROWL}
-OUTPUT=$(/usr/bin/curl "https://apps.rivierabuild.com/upload" -F file=@"${TMP_FILE_PATH}" -F availability="${AVAILABILITY}" -F passcode="${PASSWORD}") #the password parametre is optional here
+OUTPUT=$(/usr/bin/curl "https://apps.rivierabuild.com/upload" -F api_key="${API_KEY}" -F app_id="${APP_ID}" -F file=@"${TMP_FILE_PATH}" -F availability="${AVAILABILITY}" -F passcode="${PASSWORD}") #the password parametre is optional here
 URL=$(echo $OUTPUT | python -m json.tool | sed -n -e '/"file_url":/ s/^.*"\(.*\)".*/\1/p')
 echo "Uploaded .ipa to RivieraBuild" | ${GROWL}
 
